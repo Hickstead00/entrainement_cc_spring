464ebf8bdc6f16cd04b04e1894af55e1e5e977d0
Bodin Virgile <virgile.bodin@etu.univ-orleans.fr>
2026-03-24 17:05:39 +0100

    Initialisation projet

---
+++ src/test/java/com/bibliotheque/entrainement/EntrainementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bibliotheque.entrainement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntrainementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
